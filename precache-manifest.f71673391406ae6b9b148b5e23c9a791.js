17eb10d6fff7897b897052b8eb3867de0d3e4866
Timofei1 <57501337+Timofei1@users.noreply.github.com>
2020-01-30 09:04:53 +0400

    Initial commit

---
+++ precache-manifest.f71673391406ae6b9b148b5e23c9a791.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf350aad89843b9d122306817bb865f",
+    "url": "/poz/index.html"
+  },
+  {
+    "revision": "57d19304c908bfabd495",
+    "url": "/poz/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "fa736c598b93e2c2aa31",
+    "url": "/poz/static/css/main.14abd855.chunk.css"
+  },
+  {
+    "revision": "57d19304c908bfabd495",
+    "url": "/poz/static/js/2.475d182f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/poz/static/js/2.475d182f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa736c598b93e2c2aa31",
+    "url": "/poz/static/js/main.54cb89d2.chunk.js"
+  },
+  {
+    "revision": "a7321ac9ba8bc06402d3",
+    "url": "/poz/static/js/runtime-main.a6a2301f.js"
+  }
+]);
